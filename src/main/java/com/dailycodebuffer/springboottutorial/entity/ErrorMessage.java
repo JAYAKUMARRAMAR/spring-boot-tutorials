618e0ce1d926b384cfcd5e2c8f2d6539fb580d48
JAYAKUMARRAMAR <jayakumarramar92@gmail.com>
2023-07-28 13:28:20 +0530

    adding files

---
+++ src/main/java/com/dailycodebuffer/springboottutorial/entity/ErrorMessage.java
@@ -0,0 +1,30 @@
+package com.dailycodebuffer.springboottutorial.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+
+    private HttpStatus status;
+    private String message;
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+	public void setStatus(HttpStatus status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+    
+    
+}
